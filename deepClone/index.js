86b41d75288e77601406cf86a2bbde9913e3c769
jacket <1762982273@qq.com>
2020-10-29 22:14:56 +0800

    js format deepclone

--- deepClone/index.js
+++ deepClone/index.js
@@ -1,47 +1,47 @@
-function deepClone(source,map = new WeakMap()) {
-  if (source instanceof Object) {
-    let dist
-    //  对四种类型进行判断
-    if (map.get(source)) {
-      return map.get(source);
-    }
-    
-    if (source instanceof Array) {
-      dist = new Array()
-    } else if (source instanceof Function) {
-      dist = function () {
-        return source.apply(this,arguments)
-      }
-      dist.name = dist
-    } else if (source instanceof Date) {
-      dist = new Date(source)
-    } else if (source instanceof RegExp) {
-      dist = new RegExp(source.source,source.flags)
-    }else {
-      dist = new Object()
-    }
-    map.set(source, dist);
+function deepClone(source, map = new WeakMap()) {
+	if (source instanceof Object) {
+		let dist
+		//  对四种类型进行判断
+		if (map.get(source)) {
+			return map.get(source)
+		}
 
-    // 进行遍历对象 copy
-    for (const key in source) {
-      if (source.hasOwnProperty(key)) {
-        // 递归遍历,传入 map ，保证同一个
-        dist[key] = deepClone(source[key],map)
-      }
-    }
+		if (source instanceof Array) {
+			dist = new Array()
+		} else if (source instanceof Function) {
+			dist = function () {
+				return source.apply(this, arguments)
+			}
+			dist.name = dist
+		} else if (source instanceof Date) {
+			dist = new Date(source)
+		} else if (source instanceof RegExp) {
+			dist = new RegExp(source.source, source.flags)
+		} else {
+			dist = new Object()
+		}
+		map.set(source, dist)
 
-    return dist
-  }
+		// 进行遍历对象 copy
+		for (const key in source) {
+			if (source.hasOwnProperty(key)) {
+				// 递归遍历,传入 map ，保证同一个
+				dist[key] = deepClone(source[key], map)
+			}
+		}
 
-  return source
+		return dist
+	}
+
+	return source
 }
 
 const a = {
-  name: function () {
-    console.log('name')
-  },
-  date: new Date(),
-  array: [1, 2, 3],
+	name: function () {
+		console.log('name')
+	},
+	date: new Date(),
+	array: [1, 2, 3],
 }
 // 环，存在爆栈，暂不考虑
 a.a = a
