cb23bb734bf91d98d8c396861fcd7ce309e186ea
jacket <1762982273@qq.com>
2020-11-22 21:47:37 +0800

    webpack  add comment and format

--- webpack/xdml/index.ts
+++ webpack/xdml/index.ts
@@ -17,7 +17,8 @@ analysisModule('./preject1/index.js')
 console.log(depRelation)
 
 function analysisModule(filepath: string): void {
-	if (Object.keys(depRelation).includes(filepath)) {
+	// 利用 hash 对比,判断是否存在循环引入
+	if (depRelation[filepath] !== undefined) {
 		console.log(`duplicated dependency: ${filepath}`)
 		return
 	}
@@ -34,11 +35,13 @@ function analysisModule(filepath: string): void {
 	traverse(ast, {
 		enter: function (fileObj) {
 			if (fileObj.node.type === 'ImportDeclaration') {
+				// 将当前依赖模块路径推入
 				const depPath = fileObj.node.source.value
-
 				depRelation[filepath].deps.push(depPath)
 
+				// 获取当前目录
 				const dirName = path.dirname(filepath)
+				// 拼接依赖路径，递归处理依赖模块
 				const depRelativePath = `./${path.join(dirName, depPath)}`
 				analysisModule(depRelativePath)
 			}
