b59c894567b497ec89d9c875378a392b8194303b
jacket <1762982273@qq.com>
2020-11-22 17:51:34 +0800

    webpack change ast and generate code

--- webpack/xdml/let_to_var.ts
+++ webpack/xdml/let_to_var.ts
@@ -4,4 +4,17 @@ import generator from '@babel/generator'
 
 const code = `let a = 'left';let b = 2`
 const ast = parse(code, { sourceType: 'module' })
+traverse(ast, {
+	enter: (item) => {
+		if (item.node.type === 'VariableDeclaration') {
+			if (item.node.kind === 'let') {
+				item.node.kind = 'var'
+			}
+		}
+	},
+})
+
+const result = generator(ast, {}, code)
+
 console.log(ast)
+console.log(result.code)
