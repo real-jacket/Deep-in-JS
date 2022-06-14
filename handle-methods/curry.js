bee6d1ef603d4874d90073038d24a3a455496a43
real-jacket <1762982273@qq.com>
2022-06-14 17:58:11 +0800

    feat(handle method): add curry function

---
+++ handle-methods/curry.js
@@ -0,0 +1,29 @@
+const _curry = function (fn) {
+  return function (x) {
+    return function (y) {
+      return fn(x, y);
+    };
+  };
+};
+
+const _fn = (x, y) => x + y;
+
+let _myfn = _curry(_fn);
+
+console.log(_myfn(1)(2));
+
+// 多参数柯里化；
+const curry = function (fn) {
+  return function curriedFn(...args) {
+    if (args.length < fn.length) {
+      return function (...args2) {
+        return curriedFn(...args, ...args2);
+      };
+    }
+
+    return fn(...args);
+  };
+};
+const fn = (x, y, z) => x + y + z;
+const myfn = curry(fn);
+console.log(myfn(1)(2)(3));
