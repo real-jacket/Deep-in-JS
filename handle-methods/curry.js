a83979519e3e8cedcabd30e184b8289ef065b8b0
real-jacket <1762982273@qq.com>
2024-03-14 22:51:07 +0800

    feat(handle-method): new implement for curry

--- handle-methods/curry.js
+++ handle-methods/curry.js
@@ -15,6 +15,7 @@ console.log(_myfn(1)(2));
 // 多参数柯里化；
 const curry = function (fn) {
   return function curriedFn(...args) {
+    // fn.length 表示的是函数的形参个数
     if (args.length < fn.length) {
       return function (...args2) {
         return curriedFn(...args, ...args2);
@@ -27,3 +28,22 @@ const curry = function (fn) {
 const fn = (x, y, z) => x + y + z;
 const myfn = curry(fn);
 console.log(myfn(1)(2)(3));
+
+const s_curry = function (fn) {
+  let args = [];
+  function calc(..._args) {
+    args = [...args, ..._args];
+
+    if (args.length < fn.length) {
+      return calc;
+    } else {
+      return fn.apply(this, args);
+    }
+  }
+
+  return calc;
+};
+
+const s_fn = (x, y, z) => x + y + z;
+const s_myfn = curry(fn);
+console.log(s_myfn(1)(2)(3));
